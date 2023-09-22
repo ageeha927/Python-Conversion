39cb3d279c4ffc7211cbf843d60ad55a7a94e5c0
ageeha927 <141285328+ageeha927@users.noreply.github.com>
2023-09-22 16:16:36 -0700

    Update colabE&A.py

---
+++ colabE&A.py
@@ -0,0 +1,4 @@
+decimal_number = int(input("What decimal # do you want to convert into binary and hexadecimal?"))
+binary_num = bin(decimal_number)
+hexa_num = hex(decimal_number)
+print("Binary: " + binary_num + " Hexadecimal: " + hexa_num)
